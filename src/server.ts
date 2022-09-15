6192bc499553bac34e7f85ded1d5343a6b608925
gabrielle <catarinogabrielle@gmail.com>
2022-09-15 19:41:42 -0300

    atualização

--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,7 @@ app.use(cors())
 
 app.use(router);
 
-app.use(
-    '/files',
-    express.static(path.resolve(__dirname, '..', 'tmp'))
-)
+app.use("/files", express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
